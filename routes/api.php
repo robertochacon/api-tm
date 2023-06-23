c105df931839488cbc93ca34b1661fbb747509d5
rchaconalcantara <hola.hack09@gmail.com>
2023-06-23 18:04:32 -0400

    Adding routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\BooksController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,4 +30,26 @@ Route::group([
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
     Route::post('register', [AuthController::class, 'register']);
+
+    //authors
+    Route::get('/authors/', [AuthorsController::class, 'index']);
+    Route::get('/authors/{id}/', [AuthorsController::class, 'watch']);
+    Route::post('/authors/', [AuthorsController::class, 'register']);
+    Route::put('/authors/{id}/', [AuthorsController::class, 'update']);
+    Route::delete('/authors/{id}/', [AuthorsController::class, 'delete']);
+
+    //categories
+    Route::get('/categories/', [CategoriesController::class, 'index']);
+    Route::get('/categories/{id}/', [CategoriesController::class, 'watch']);
+    Route::post('/categories/', [CategoriesController::class, 'register']);
+    Route::put('/categories/{id}/', [CategoriesController::class, 'update']);
+    Route::delete('/categories/{id}/', [CategoriesController::class, 'delete']);
+
+    //books
+    Route::get('/books/', [BooksController::class, 'index']);
+    Route::get('/books/{id}/', [BooksController::class, 'watch']);
+    Route::post('/books/', [BooksController::class, 'register']);
+    Route::put('/books/{id}/', [BooksController::class, 'update']);
+    Route::delete('/books/{id}/', [BooksController::class, 'delete']);
+
 });
