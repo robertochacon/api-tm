b6a1dede1a8a8c75e1c56178bdbbee7e3a24f452
rchaconalcantara <hola.hack09@gmail.com>
2023-06-23 19:23:10 -0400

    Adding seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['librarian','admin'])->default('librarian');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        //adding users
+        DB::table('users')->insert([
+            ['name' => 'Admin','email' => 'admin@gmail.com','password' => bcrypt('admin'),'role' => 'admin'],
+            ['name' => 'Librarian','email' => 'librarian@gmail.com','password' => bcrypt('librarian'),'role' => 'librarian']
+        ]);
+        
+        //adding authors
+        DB::table('authors')->insert([
+            ['name' => 'Carmen Natalia'],
+            ['name' => 'Manuel de Jesús Galván'],
+            ['name' => 'Mario Vargas Llosa']
+        ]);
+
+        //adding categories
+        DB::table('categories')->insert([
+            ['name' => 'Ficción'],
+            ['name' => 'Novela histórica']
+        ]);
+
+        //adding books
+        DB::table('books')->insert([
+            ['author_id' => 1,'category_id' => 1,'title' => 'La victoria','description' => ' La novela, originada en 1942, no narra una historia nueva e innovadora, sino el amor que sienten dos hermanos por una misma mujer.'],
+            ['author_id' => 2,'category_id' => 1,'title' => 'Enriquillo','description' => 'Enriquillo es una novela histórica dominicana escrita por Manuel de Jesús Galván y publicada entre 1879 y 1882. Ubicada en el siglo XVI.'],
+            ['author_id' => 3,'category_id' => 2,'title' => 'La fiesta del Chivo','description' => 'La fiesta del Chivo es una novela publicada en el año 2000 del escritor peruano-español Mario Vargas Llosa. El libro tiene lugar en República Dominicana.'],
+        ]);
+
     }
 }
